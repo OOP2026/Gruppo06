a1eef641959846b69da96124009b1a954e6cf70b
stefanomontella <unistefanomontella@gmail.com>
2026-05-08 12:22:24 +0200

    Creazione della GUI

--- src/main/java/gui/Home.java
+++ src/main/java/gui/Home.java
@@ -1,8 +1,27 @@
 package gui;
 
+import javax.swing.*;
+
 public class Home {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Home");
+        frame.setContentPane(new Home().panelHome);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setSize(1000, 800); // Imposta le dimensioni desiderate
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null); // Centra la finestra
+        frame.setVisible(true);
+    }
 
-	}
-
+    private JPanel panelHome;
+    private JButton pazientiButton;
+    private JButton lettiButton;
+    private JButton dimissioniButton;
+    private JButton mediciButton;
+    private JButton turniButton;
+    private JButton prestazioniButton;
+    private JButton ricoveroButton;
 }

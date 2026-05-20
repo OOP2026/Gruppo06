dbf2ead33a9da4e409dd32a86802c33327120197
Stefano Montella <stefan.montella@studenti.unina.it>
2026-05-20 13:58:36 +0200

    Continuo GUI
    Interfaccia Schermata_Medico

--- src/main/java/gui/Schermata_Medico.java
+++ src/main/java/gui/Schermata_Medico.java
@@ -1,4 +1,14 @@
 package gui;
 
+import javax.swing.*;
+
 public class Schermata_Medico {
+    private JButton ricoveroButton;
+    private JButton turniButton;
+    private JLabel utenteLoggatoLabel;
+    private JButton esciButton;
+    private JButton pazientiButton;
+    private JButton lettiButton;
+    private JButton dimissioniButton;
+    private JButton prestazioniButton;
 }
